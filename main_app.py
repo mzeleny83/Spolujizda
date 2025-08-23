98879a8b75147259ae250c6bbeb4d0d2909f4c9b
mzeleny83 <110114227+mzeleny83@users.noreply.github.com>
2025-08-23 10:28:59 +0200

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -1536,7 +1536,7 @@ def create_checkout_session():
         conn.close()
         
         # Platba pouze provize na tvůj účet, zbytek v hotovosti
-        paypal_url = f'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=m.zeleny@volny.cz&item_name=Poplatek Sveztese.cz #{ride_id}&amount={commission}&currency_code=CZK&return={request.host_url}payment-success?ride_id={ride_id}&amount={amount}&commission={commission}&cancel_return={request.host_url}payment-cancel'
+        paypal_url = f'https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=m.zeleny@volny.cz&item_name=Poplatek+Sveztese.cz+#{ride_id}&amount={commission}&currency_code=CZK&return={request.host_url}payment-success?ride_id={ride_id}&amount={amount}&commission={commission}&cancel_return={request.host_url}payment-cancel'
         
         return jsonify({'checkout_url': paypal_url}), 200
         
